e2c03e3dcef87c41f9da4ee3ccdb4d1172f0cd7c
Shawn <43576702+saclark12000@users.noreply.github.com>
2019-10-23 10:18:28 -0500

    added first unit test

---
+++ BananaStore/BananaStore.Tests/OrdersTests.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Xunit;
+using BananaStore.DataAccess.Entities;
+using BananaStore.DataAccess.Repositories;
+using BananaStore.Library.Interfaces;
+
+
+namespace BananaStore.Tests
+{
+    public class OrdersTests
+    {
+
+        private readonly Orders _orders = new Orders();
+
+        [Fact]
+        public void Name_NonEmptyValue_StoresCorrectly()
+        {
+             Assert.IsType<Guid>(_orders.OrderId);
+
+        }
+    }
+}
